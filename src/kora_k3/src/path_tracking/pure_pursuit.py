2a6c863953f460eed9b9fa20194da567ce78a618
hajaemingood <hjm021123@gmail.com>
2025-11-05 09:34:51 +0900

    parameter test

--- src/kora_k3/src/path_tracking/pure_pursuit.py
+++ src/kora_k3/src/path_tracking/pure_pursuit.py
@@ -18,7 +18,7 @@ class Pure_pursuit:
         self.min_speed = 1.0
         self.alpha = 1.5
 
-        self.csv_file = "/root/KORA_K3/src/kora_k3/src/path_planning/outputs/optimal_waypoints.csv"
+        self.csv_file = "/root/KORA_K3/src/kora_k3/src/path_planning/outputs/waypoints.csv"
         self.waypoints = self.load_waypoints()
         self.curvature = 0.0
 
